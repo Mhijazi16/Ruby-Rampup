5b6a2e5aed27686fcec5487f733f92d4fdcc96a2
Mhijazi16 <gultimate0@gmail.com>
2024-09-08 22:55:27 +0300

    implementing binary serach

---
+++ binary_serach.rb
@@ -0,0 +1,21 @@
+
+array = [1,2,3,4,5,6,7]
+
+left = 0
+right = array.size() 
+
+target = 6
+
+while left <= right
+  mid = left + (right - left)/2
+  if array[mid] == target
+    print "found the target at ",mid, "\n"
+    break
+  elsif array[mid] > target 
+    right = mid - 1
+  else 
+    left = mid + 1
+  end
+end
+
+
