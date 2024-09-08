ff31c9daea1f0aa43443b9f4891c839e9a19de31
Mhijazi16 <gultimate0@gmail.com>
2024-09-08 22:46:17 +0300

    uncommenting stuff

--- first.rb
+++ first.rb
@@ -1,23 +1,27 @@
-# array2 = 1...14
-#
-# for value in array2 
-#   print value, " "
-# end
-#
 
-# 10.times do 
-#   print "this will appear 10 time \n"
-# end 
+# looping over array
+array2 = 1...14
+for value in array2 
+  print value, " "
+end
 
-# [1,2,3,4,5,5].each do |item| 
-#   print item, " "
-# end 
+# new way of looping
+10.times do 
+  print "this will appear 10 time \n"
+end 
 
-# for i in 1..10
-#   next if i % 2 == 0
-#   print i, "is an odd number\n"
-# end 
+#looping from an array
+[1,2,3,4,5,5].each do |item| 
+  print item, " "
+end 
 
+#jumping inside the loop
+for i in 1..10
+  next if i % 2 == 0
+  print i, "is an odd number\n"
+end 
+
+#looping over a hashmap
 map = {"one" => 1 , "two" => 2, "three" => 3} 
 map.each do |x,y|
   print "Key: ", x, ", Value: ", y, "\n"
