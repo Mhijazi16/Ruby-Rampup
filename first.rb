52563bdcd293daa88ea1a926fbc16ede8218928f
Mhijazi16 <gultimate0@gmail.com>
2024-09-08 22:43:59 +0300

    loop over a hashmap

--- first.rb
+++ first.rb
@@ -13,7 +13,12 @@
 #   print item, " "
 # end 
 
-for i in 1..10
-  next if i % 2 == 0
-  print i, "is an odd number\n"
-end 
+# for i in 1..10
+#   next if i % 2 == 0
+#   print i, "is an odd number\n"
+# end 
+
+map = {"one" => 1 , "two" => 2, "three" => 3} 
+map.each do |x,y|
+  print "Key: ", x, ", Value: ", y, "\n"
+end
