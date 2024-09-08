57aaf19aa21f92a759a767c45c1e2e655fd5dd5f
Mhijazi16 <gultimate0@gmail.com>
2024-09-08 22:36:48 +0300

    new ways of looping

--- first.rb
+++ first.rb
@@ -1,5 +1,9 @@
-array2 = 1...14
+# array2 = 1...14
+#
+# for value in array2 
+#   print value, " "
+# end
 
-for value in array2 
-  print value, " "
-end
+10.times do 
+  print "this will appear 10 time \n"
+end 
