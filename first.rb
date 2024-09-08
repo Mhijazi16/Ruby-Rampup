104c791a6a9c0e12c02195a3263d1d0142649815
Mhijazi16 <gultimate0@gmail.com>
2024-09-08 22:39:11 +0300

    another way of looping

--- first.rb
+++ first.rb
@@ -3,7 +3,12 @@
 # for value in array2 
 #   print value, " "
 # end
+#
 
-10.times do 
-  print "this will appear 10 time \n"
+# 10.times do 
+#   print "this will appear 10 time \n"
+# end 
+
+[1,2,3,4,5,5].each do |item| 
+  print item, " "
 end 
