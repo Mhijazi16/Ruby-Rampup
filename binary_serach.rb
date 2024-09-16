b1920fc70224962ae71b7590a780ce845083369a
Mhijazi16 <gultimate0@gmail.com>
2024-09-17 00:00:37 +0300

    some fixes

--- binary_serach.rb
+++ binary_serach.rb
@@ -1,21 +1,18 @@
-
-array = [1,2,3,4,5,6,7]
+array = [1, 2, 3, 4, 5, 6, 7]
 
 left = 0
-right = array.size() 
+right = array.size
 
 target = 6
 
 while left <= right
-  mid = left + (right - left)/2
+  mid = left + (right - left) / 2
   if array[mid] == target
-    print "found the target at ",mid, "\n"
+    print 'found the target at', mid, "\n"
     break
-  elsif array[mid] > target 
+  elsif array[mid] > target
     right = mid - 1
-  else 
+  else
     left = mid + 1
   end
 end
-
-
